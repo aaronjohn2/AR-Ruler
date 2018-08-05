76c7f8c5f7d170be6185a6bfec01ddada0196fef
Aaron John <aaronjohn@aarons-macbook-pro.local>
2018-08-04 18:29:47 -0700

    Initial Commit

---
+++ Ruler 3D AR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ruler 3D AR
+//
+//  Created by Aaron John on 8/4/18.
+//  Copyright © 2018 Aaron John. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
